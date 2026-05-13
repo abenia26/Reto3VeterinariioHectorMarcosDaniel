3ea2cc218d3793c49e02eb36a7d3d4a42a88cf2c
1DAM <1DAM@A11-PC21>
2026-05-13 13:15:44 +0200

    1

---
+++ RetoVeterinarioHectorMarcosDaniel/src/dao/ClienteDAO.java
@@ -0,0 +1,148 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+
+import modelo.Cliente;
+import util.ConexionBD;
+
+public class ClienteDAO implements GenericDAO<Cliente> {
+
+	@Override
+	public boolean insertar(Cliente cliente) {
+		String sql ="INSERT INTO clientes (id_persona,telefono) VALUES (?, ?)"; 
+
+
+		try (Connection conn = ConexionBD.getConnection();
+		PreparedStatement pstmt = conn.prepareStatement(sql,               Statement.RETURN_GENERATED_KEYS)) {
+
+
+		
+			pstmt.setInt(1, cliente.getId_persona());
+			pstmt.setString(2, cliente.getTelefono());
+		
+
+
+			int filas = pstmt.executeUpdate();
+
+
+			if (filas > 0) {
+				try (ResultSet rs = pstmt.getGeneratedKeys()) {
+					if (rs.next()) {
+					cliente.setId_cliente(rs.getInt(1)); // asigna el ID
+						return true;
+					}
+				}
+			}
+			return false;
+
+
+		} catch (SQLException e) {
+			System.err.println("Error SQL al insertar '" + cliente.getId_cliente() + "': " + e.getMessage());
+			return false;
+		}
+	}
+
+	@Override
+	public List<Cliente> obtenerTodos() {
+		List<Cliente> clientes = new ArrayList<>();
+	    String sql = "SELECT id_cliente,id_persona,telefono FROM clientes ORDER BY id_cliente";
+
+
+			try (Connection conn = ConexionBD.getConnection();
+					PreparedStatement pstmt = conn.prepareStatement(sql);
+					ResultSet rs = pstmt.executeQuery()) {
+
+
+				while (rs.next()) {
+					clientes.add(mapearFila(rs));
+				}
+
+
+			} catch (SQLException e) {
+				System.err.println("Error SQL al obtener todos los clientes: " + e.getMessage());
+			}
+			return clientes;
+	}
+
+	@Override
+	public Cliente obtenerPorId(int id) {
+		 String sql = "SELECT id_cliente,id_persona,telefono FROM clientes WHERE id = ?";
+
+
+			try (Connection conn = ConexionBD.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+
+				pstmt.setInt(1, id);
+
+
+				try (ResultSet rs = pstmt.executeQuery()) {
+					if (rs.next()) {
+						return mapearFila(rs);
+					}
+				}
+
+
+			} catch (SQLException e) {
+				System.err.println("Error SQL al buscar ID " + id + ": " + e.getMessage());
+			}
+			return null; // 
+	}
+
+	@Override
+	public boolean actualizar(Cliente cliente) {
+		String sql = "UPDATE clientes SET id_persona=?, telefono=? WHERE id=?";
+
+
+		try (Connection conn = ConexionBD.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+
+			pstmt.setInt(1, cliente.getId_persona());
+			pstmt.setString(2, cliente.getTelefono());
+			
+
+
+			int filas = pstmt.executeUpdate();
+			return filas > 0; // false si el ID no existía en la BD
+
+
+		} catch (SQLException e) {
+			System.err.println("Error SQL al actualizar ID " + cliente.getId_cliente() + ": " + e.getMessage());
+			return false;
+		}
+	}
+
+	@Override
+	public boolean eliminar(int id) {
+		String sql = "DELETE FROM clientes WHERE id=?";
+
+
+		try (Connection conn = ConexionBD.getConnection(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
+
+
+			pstmt.setInt(1, id);
+			int filas = pstmt.executeUpdate();
+			return filas > 0; // false si el ID no existía
+
+
+		} catch (SQLException e) {
+			System.err.println("Error SQL al eliminar ID " + id + ": " + e.getMessage());
+			return false;
+		}
+	}
+	private Cliente mapearFila(ResultSet rs) throws SQLException {
+		Cliente a = new Cliente();
+		a.setId_cliente(rs.getInt("id_cliente"));
+		a.setId_persona(rs.getInt("id_persona"));
+		a.setTelefono(rs.getString("telefono"));
+		return a;
+	}
+
+
+}
---
+++ RetoVeterinarioHectorMarcosDaniel/src/modelo/Persona.java
@@ -0,0 +1,44 @@
+package modelo;
+
+public class Persona {
+protected int id_persona;
+protected String dni;
+protected String nombre;
+public Persona(int id_persona, String dni, String nombre) {
+	super();
+	this.id_persona = id_persona;
+	this.dni = dni;
+	this.nombre = nombre;
+}
+public Persona(String dni, String nombre) {
+	super();
+	this.dni = dni;
+	this.nombre = nombre;
+}
+public Persona() {
+	super();
+}
+public int getId_persona() {
+	return id_persona;
+}
+public void setId_persona(int id_persona) {
+	this.id_persona = id_persona;
+}
+public String getDni() {
+	return dni;
+}
+public void setDni(String dni) {
+	this.dni = dni;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+@Override
+public String toString() {
+	return "Persona [id_persona=" + id_persona + ", dni=" + dni + ", nombre=" + nombre + "]";
+}
+
+}
